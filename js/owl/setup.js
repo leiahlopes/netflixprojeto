a80e3d617e0db8d3d0df9e6bff0b64940e38568b
Vanderleia Lopes <83191034+leiahlopes@users.noreply.github.com>
2021-05-06 17:10:20 -0300

    projeto entregue

---
+++ js/owl/setup.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        o: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+
+})
